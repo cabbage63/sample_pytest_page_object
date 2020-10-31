25e263f549c627874742959808cfa3df75fac411
m.kurahashi <7237868+cabbage63@users.noreply.github.com>
2020-10-31 15:36:25 +0900

    resolved

--- Pages/b_page.py
+++ Pages/b_page.py
@@ -1,8 +1,9 @@
 # related page objects
 from Pages.base_page import BasePage
-from Pages.a_page import *
+
 
 class bPage(BasePage):
     def go_to_a_page(self):
         return aPage()
 
+from Pages.a_page import *
